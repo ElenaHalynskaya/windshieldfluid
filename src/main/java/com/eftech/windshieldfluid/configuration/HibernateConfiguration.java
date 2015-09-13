65502563bd59f29cb20fbcf3d74415d64a8ecd75
ElenaHalynskaya <elena_30033003@rambler.ru>
2015-09-13 23:27:45 +0300

    Add properties and hivernateconfig class

---
+++ src/main/java/com/eftech/windshieldfluid/configuration/HibernateConfiguration.java
@@ -0,0 +1,66 @@
+package com.eftech.windshieldfluid.configuration;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.ejb.HibernatePersistence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "com.iftech.windshieldfluid.configuration" })
+@PropertySource(value = { "classpath:application.properties" })
+
+public class HibernateConfiguration {
+    
+    @Autowired
+    private Environment environment; 
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return dataSource;
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+	entityManagerFactoryBean.setDataSource(dataSource());
+	entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+	entityManagerFactoryBean.setPackagesToScan(new String[] { "com.iftech.windshieldfluid.model" });
+	
+	entityManagerFactoryBean.setJpaProperties(hibernateProperties());
+	
+	return entityManagerFactoryBean;
+	}
+        
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        return properties;        
+    }
+    
+    @Bean
+    public JpaTransactionManager transactionManager() {
+	JpaTransactionManager transactionManager = new JpaTransactionManager();
+	transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+	return transactionManager;
+    }
+    
+    
+}
